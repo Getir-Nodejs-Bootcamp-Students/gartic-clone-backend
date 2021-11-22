342645c7947cb98cb5e767bb8ce378a196aa1ff4
Emre <imamogluemree@gmail.com>
2021-11-22 22:12:46 +0300

    Add template events for triggering socket transmissions

--- socket/socket.js
+++ socket/socket.js
@@ -1,8 +1,11 @@
 const { joinRoom, sendMessage, drawCanvas } = require("./socketHandler");
+var events = require('events');
 var socket_io = require("socket.io");
+const { EventEmitter } = require("stream");
 var io = socket_io();
 var socketApi = {};
 var connectedUsers = {
+  
 };
 socketApi.io = io;
 
@@ -50,5 +53,12 @@ io.on("connection", function (socket) {
 socketApi.sendNotification = function () {
   io.sockets.emit("hello", { msg: "Hello World!" });
 };
+
+var eventEmmiter = new events.EventEmitter();
+eventEmmiter.on('turn:change',()=>{})
+eventEmmiter.on('score:add',()=>{})
+eventEmmiter.on('time:start',()=>{})
+
+
 
 module.exports = socketApi;
